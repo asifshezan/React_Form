05e6a633a0e3bdf607dca2b312e7b971299501bf
Asif Ahamed <asifshezan7@gmail.com>
2022-08-25 21:57:39 +0600

    form handle useState with sprade operator

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -14,17 +14,24 @@ function Form (){
 
 
         const handleChange = (e) => {
-        const fieldName = e.target.name;
-        if(fieldName === 'name'){
-            setUser({name:e.target.value, email, phone, password});
-        }else if(fieldName === 'email'){
-            setUser({name, email:e.target.value, phone, password});
-        }else if(fieldName === 'phone'){
-            setUser({name, email, phone:e.target.value, password});
-        }else if(fieldName === 'password'){
-            setUser({name, email, phone, password:e.target.value});
-        }
-        }
+            setUser({ ...user, [e.target.value]:e.target.value });
+        };
+
+
+        // const handleChange = (e) => {
+        //     const fieldName = e.target.name;
+        //     if(fieldName === 'name'){
+        //         setUser({name:e.target.value, email, phone, password});
+        //     }else if(fieldName === 'email'){
+        //         setUser({name, email:e.target.value, phone, password});
+        //     }else if(fieldName === 'phone'){
+        //         setUser({name, email, phone:e.target.value, password});
+        //     }else if(fieldName === 'password'){
+        //         setUser({name, email, phone, password:e.target.value});
+        //     }
+        //     }
+
+        
         
 
         const handleSubmit = (e) => {
@@ -32,9 +39,10 @@ function Form (){
         console.log(user);
         e.preventDefault();
         };
+
+
     return(
-        <div>
-        
+        <div>  
             <Card style={{width:"700px", textAlign: "center", marginleft:"400px"}}>
             <Card.Header className={style.heading1}>Registration Form</Card.Header>
             <Card.Body>
